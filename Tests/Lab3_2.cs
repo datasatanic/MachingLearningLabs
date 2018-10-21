75a81615f4575279a4b9e8489df4795b46c205a4
OgolihinIlya <Ogolihin1503@gmail.com>
2018-10-21 19:23:20 +0500

    Add Test for Lab3_2

---
+++ Tests/Lab3_2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lab1Tests1
+{
+    [TestClass]
+    public class Lab3_2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //TODO:Сделать тесты для Lab3_2
+            }
+    }
+}
