91b92f1cf3b8c4fd4a6d25670e56fc8d61e702cd
OgolihinIlya <Ogolihin1503@gmail.com>
2018-10-31 20:15:14 +0500

    Работал над Lab5_1

--- Lab5/Classes.py
+++ Lab5/Classes.py
@@ -1,5 +1,5 @@
 from numpy import *
-
+import matplotlib.pyplot as pl
 class Hopfield:
     """Сеть Хопфилда"""
     def __init__(self,input):
@@ -52,5 +52,32 @@ class Hopfield:
         res=tuple(map(lambda x: x.tolist()[0][0],y2)) 
         ind,max=self.__Equal(res)      
         print("Элемент\n {0} \n принадлежит классу\n {1}\n c индексом={2} c вероятностью {3:.2%}\n".format(self.__FormatString(check),self.__FormatString(self.input[ind]),ind,max))
+
+class Associative:
+   def __init__(self,input):
+       self.input=input
+       self.Weight=self.GetWeight(input)
+
+   def GetWeight(self,input):
+       l=list(input.values())[0]
+       W=zeros((len(l)*len(l[0]),len(input)))
+       for item in input:
+           m=matrix(input[item])
+           W+=dot(transpose(reshape(m,m.size)),matrix(item))
+       return W
+
+   def Compute(self,check):
+       m=matrix(check)
+       m=reshape(m,m.size)
+       x1=sign(dot(m,self.Weight))
+       y1=sign(dot(x1,transpose(self.Weight)))
+       x2=sign(dot(y1,self.Weight))
+       y2=sign(dot(x2,transpose(self.Weight)))
+       while not (x1==x2).all() and not (y1==y2).all():
+           y1=y2
+           x1=x2
+           y1=sign(dot(y1,transpose(self.Weight)))
+           y2=sign(dot(x2,transpose(self.Weight)))
+       return (x2,y2)
 
 
