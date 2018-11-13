f77387a0a29a6490b72cc5e24297191a12cb623a
OgolihinIlya <Ogolihin1503@gmail.com>
2018-11-13 17:16:04 +0500

    Complete

--- Lab6/Classes.py
+++ Lab6/Classes.py
@@ -9,17 +9,24 @@ class Hemming:
         self.V=np.identity(len(input))-self.eps+np.identity(len(input))*self.eps
 
     def func(self,vect):
-        for i in vect:
-            if(i<=0):
-                i=0
-            elif(i>=T):
-                i=T
+        for i in range(len(vect)):
+            if(vect[i]<=0):
+                vect[i]=0
+            elif(vect[i]>=self.T):
+                vect[i]=self.T
         return vect
+
+    def res(self,vect):
+        if np.count_nonzero(vect)>1:
+            return "Класс не распознан"
+        else:
+            return "Объект принадлежит классу %d"%(np.flatnonzero(vect)[0]+1)
                 
     def compute(self,vect):
         y1=self.func(np.dot(self.W,vect)+self.T)
         y2=self.func(np.dot(self.V,y1))
-        while np.linalg.norm(y1,y2)>self.eps:
+        while np.linalg.norm(y2-y1)>self.eps:
             y1=y2
             y2=self.func(np.dot(self.V,y1))
+        return self.res(y2)
         
--- Lab6/Lab6.py
+++ Lab6/Lab6.py
@@ -6,10 +6,13 @@ input=np.array([
     [1,1,1,1,-1,1,1,1,1]])
 
 vect=np.array([1,-1,-1,-1,1,-1,1,-1,1])
+vect1=np.array([1,1,1,-1,-1,-1,1,1,1])
+
+
 
 def main():
     net=Hemming(input,0.3)
     print(net.compute(vect))
 
 if __name__ == "__main__":
-    sys.exit(int(main() or 0))
+    main()
