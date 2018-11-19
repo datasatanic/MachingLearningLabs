c45a2edb97e7e21d6cdb8217aaf513042ba395b5
OgolihinIlya <Ogolihin1503@gmail.com>
2018-11-20 02:27:31 +0500

    Lab7

--- Lab7/Lab7.py
+++ Lab7/Lab7.py
@@ -1,1 +1,18 @@
+import sys
+from Classes import *
+from datetime import datetime
+
+def main():
+    net=ConvolutionNetwork()
+    net.Load()
+    net.LoadData()
+    net.Fit()
+    net.Save()
+    input("Окончание обучения: %s"%datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M:%S"))#75%
+
+if __name__ == "__main__":
+    sys.exit(int(main() or 0))
+
+
+
 
